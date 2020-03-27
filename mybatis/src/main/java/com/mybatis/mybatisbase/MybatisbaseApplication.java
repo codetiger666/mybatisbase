af49f0ed33e98117f95546f4550cb0233f1a6541
nnweb <nnweb>
2020-03-27 19:59:21 +0800

    mybatis-plus

---
+++ mybatis/src/main/java/com/mybatis/mybatisbase/MybatisbaseApplication.java
@@ -0,0 +1,13 @@
+package com.mybatis.mybatisbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisbaseApplication.class, args);
+    }
+
+}
---
+++ mybatis/src/test/java/com/mybatis/mybatisbase/MybatisbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybatis.mybatisbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisbaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
